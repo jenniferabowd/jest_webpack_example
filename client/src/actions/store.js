2385584537f393f2a0013b47cf044f511aa29946
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2022-10-02 21:43:31 -0400

    removing references to eslint and some folder that doesn not exist in the repo

--- client/src/actions/store.js
+++ client/src/actions/store.js
@@ -24,13 +24,11 @@ export default function configureStore(initialState = {}, history) {
   ];
 
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
-  /* eslint-disable no-underscore-dangle */
   const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
-  /* eslint-enable */
 
   const store = createStore(
     createReducer(),
